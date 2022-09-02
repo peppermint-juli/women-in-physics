e2c681ce55a11b39319c2b9a83acd9ee2e9030e4
Juliana Jaime <julianajaime@Julianas-MacBook-Pro.local>
2022-09-02 13:23:49 -0400

    First push for Women In Physics JHU website

---
+++ components/ComingSoon.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      <h1 class="text-h4 font-weight-thin mb-4">
+        Women in Physics JHU
+      </h1>
+      <h4 class="subheading">
+        Coming Soon!
+      </h4>
+    </div>
+  </v-parallax>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <ComingSoon />
+    </div>
+</template>
